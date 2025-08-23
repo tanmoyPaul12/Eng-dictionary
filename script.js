6eea055a0c6becf02daf211319f93a8d75a861dd
tanmoyPaul12 <tanmoy01paul@gmail.com>
2025-08-23 19:15:49 +0530

    js done

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const inputEle = document.getElementById("input")
 const infoEle = document.getElementById("info")
 const meaningEle = document.getElementById("meaning-container")
+const wordTitleEle = document.getElementById("title")
+const wordmeaningEle = document.getElementById("meaning-txt")
+const audioEle =document.getElementById("audio")
+
+
 // promise==>
 // const fetchAPI = (word)=>{
 //     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}` 
@@ -8,6 +13,7 @@ const meaningEle = document.getElementById("meaning-container")
 //     .then((res)=>res.json());
 //     console.log(result) 
 // }
+
 
 // async await==>
 async function fetchAPI(word) {
@@ -19,16 +25,31 @@ async function fetchAPI(word) {
         const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
         let response = await fetch(url);
         let data = await response.json();
+
+        if(data.title){
+             meaningEle.style.display = "block";
+             infoEle.style.display = "none";// meaning aibo pore eta jaibo giya
+             wordTitleEle.innerText = word;
+             wordmeaningEle.innerText = "N/A";
+             audioEle.style.display = "none";
+        }  
+         else{
+             infoEle.style.display = "none"; // ans aile jate eta jay giya 
+             meaningEle.style.display = "block"; // css a none kora ase 
+             audioEle.style.display = "inline-flex";
+
+             wordTitleEle.innerText = data[0].word;
+             wordmeaningEle.innerText = data[0].meanings[0].definitions[0].definition;
+             audioEle.src = data[0].phonetics[0].audio;
+             console.log(data);
+        }
         
-        infoEle.style.display = "none"; // ans aile jate eta jay giya 
-        meaningEle.style.display = "block"; // css a none kora ase 
-        console.log(data);
+       
     } catch (error) {
-        console.log(error)
+        infoEle.innerText = `404 Error, try again later`; 
+
     }
 }
-
-
 
 
 inputEle.addEventListener("keyup" ,(e)=>{
